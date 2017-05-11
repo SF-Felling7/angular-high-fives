204fbddcf5eb2b558664dc31eff915112327c75b
Sean Felling <Snfelling7@gmail.com>
2017-05-11 13:40:14 -0500

    picture and button on the dom

---
+++ server/public/scripts/client.js
@@ -0,0 +1,7 @@
+var HighFiveApp = angular.module( 'HighFives', []);
+
+HighFives.controller( 'TheHighFives', function( $http, HighFiveApp  ) {
+
+this.MyPic = "http://img3.wikia.nocookie.net/__cb20120318215217/mario/images/1/1a/High_five.png";
+
+});
